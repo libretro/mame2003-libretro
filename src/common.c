a665e69eee79734c921f042d3330be1b0fc1ba90
grant <grant@debian.ice>
2018-05-09 16:20:33 +0100

    add back the check requested by dankcushions

--- src/common.c
+++ src/common.c
@@ -243,7 +243,7 @@ struct GameSamples *readsamples(const char **samplenames,const char *basename)
 	int skipfirst = 0;
 
 	/* if the user doesn't want to use samples, bail */
-	/* if (!options.use_samples) return 0; */
+	if (!options.use_samples) return 0; 
 
 	if (samplenames == 0 || samplenames[0] == 0) return 0;
 
