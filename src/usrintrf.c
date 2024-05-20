6b2f3d4d795a4d84bb461b9656c5b30c6f39c85b
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-05-20 08:30:56 -0400

    Update usrintrf.c

--- src/usrintrf.c
+++ src/usrintrf.c
@@ -1055,7 +1055,7 @@ static void showcharset(struct mame_bitmap *bitmap)
 	pen_t *colortable = NULL;
 	static const struct rectangle fullrect = { 0, 10000, 0, 10000 };
 
-  
+
 		/* mark the whole thing dirty */
 		ui_markdirty(&fullrect);
 
@@ -3326,6 +3326,8 @@ int handle_user_interface(struct mame_bitmap *bitmap)
 			skip_tmap = 0;
 			tilemap_xpos = 0;
 			tilemap_ypos = 0;
+
+			cpu_pause(true);
 		}
 	}
 
