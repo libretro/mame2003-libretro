51ebc80405892ba8f215feccf2936c89e7a95cb7
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2024-05-21 03:53:18 -0400

    Frameskip fix - grant2258

--- src/mame2003/mame2003.c
+++ src/mame2003/mame2003.c
@@ -110,15 +110,15 @@ static void retro_audio_buff_status_cb(bool active, unsigned occupancy, bool und
 
 void retro_set_audio_buff_status_cb(void)
 {
-  if (options.frameskip >0 && options.frameskip >= 12)
+  log_cb(RETRO_LOG_INFO, "options.frameskip:%d\n",options.frameskip);
+  if (options.frameskip > 0 && options.frameskip >= 12)
   {
       buf_status_cb.callback = &retro_audio_buff_status_cb;
 
       if (!environ_cb(RETRO_ENVIRONMENT_SET_AUDIO_BUFFER_STATUS_CALLBACK,
             &buf_status_cb))
       {
-         if (log_cb)
-            log_cb(RETRO_LOG_WARN, "Frameskip disabled - frontend does not support audio buffer status monitoring.\n");
+         log_cb(RETRO_LOG_WARN, "Frameskip disabled - frontend does not support audio buffer status monitoring.\n");
 
          retro_audio_buff_active    = false;
          retro_audio_buff_occupancy = 0;
@@ -126,7 +126,7 @@ void retro_set_audio_buff_status_cb(void)
       }
       else
       log_cb(RETRO_LOG_INFO, "Frameskip Enabled\n");
-  }
+   }
    else
       environ_cb(RETRO_ENVIRONMENT_SET_AUDIO_BUFFER_STATUS_CALLBACK,NULL);
 
